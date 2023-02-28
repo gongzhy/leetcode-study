309f284deb55caf413152db8056c089ba4d506d2
gongzhy <Gongzhy7@gmail.com>
2023-02-28 22:30:47 +0800

    1

--- src/main/java/com/gongzhy/leetcode/problems/dynamic/FibonacciNumber.java
+++ src/main/java/com/gongzhy/leetcode/problems/dynamic/FibonacciNumber.java
@@ -1,65 +1,82 @@
 package com.gongzhy.leetcode.problems.dynamic;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 斐波那契数 （通常用 F(n) 表示）形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
- * <p>
- * F(0) = 0，F(1) = 1
- * F(n) = F(n - 1) + F(n - 2)，其中 n > 1
- * 给定 n ，请计算 F(n) 。
- * <p>
- *  
- * <p>
- * 示例 1：
- * <p>
- * 输入：n = 2
- * 输出：1
- * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1
- * 示例 2：
- * <p>
- * 输入：n = 3
- * 输出：2
- * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2
- * 示例 3：
- * <p>
- * 输入：n = 4
- * 输出：3
- * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3
- *  
- * <p>
- * 提示：
- * <p>
- * 0 <= n <= 30
- * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/fibonacci-number
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class FibonacciNumber {
-    public static void main(String[] args) {
-        System.out.println(fib(3));
+
+    @Test
+    public void test() {
+        System.out.println(memorandum(31));
+        System.out.println(dp(31));
     }
 
 
-    public static int fib(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+    public int fib(int n) {
+        //
+        return dp(n);
+    }
+
+
+    /**
+     * 解法一：备忘录（自上而下）
+     *
+     * @param n
+     * @return
+     */
+    private Map<Integer, Integer> map;
+
+    private int memorandum(int n) {
+        if (n == 0 || n == 1) {
+            return n;
         }
-        int[] fibs = new int[]{0, 1};
-        return handle(fibs, 1, n);
+        map = new HashMap();
+        map.put(0, 0);
+        map.put(1, 1);
+        //
+        return memorandumHandle(n);
     }
 
-
-    private static int handle(int[] fibs, int index, int result) {
-        int ret = fibs[0] + fibs[1];
-        fibs[0] = fibs[1];
-        fibs[1] = ret;
-        index++;
-        if (index == result) {
-            return ret;
-        } else {
-            return handle(fibs, index, result);
+    /**
+     * @param n
+     * @return
+     */
+    private int memorandumHandle(int n) {
+        if (map.containsKey(n)) {
+            return map.get(n);
         }
+        int result = memorandumHandle(n - 1) + memorandumHandle(n - 2);
+        map.put(n, result);
+        return result;
     }
+
+    /**
+     * 解法二：DP 自下而上
+     *
+     * @param n
+     * @return
+     */
+    private int dp(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int fir = 0;
+        int sec = 1;
+        //
+        for (int i = 2; i <= n; i++) {
+            int sum = fir + sec;
+            fir = sec;
+            sec = sum;
+        }
+        return sec;
+    }
+
 }
